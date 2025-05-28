f3084029c97766b3a2ba224cc23be3c554c348fc
thiagopenalva <thiagopenalva@hotmail.com>
2025-05-28 20:50:25 -0300

    feat: add custom error message for existing user

--- src/app/authentication/components/sing-up-form.tsx
+++ src/app/authentication/components/sing-up-form.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -57,6 +58,14 @@ const SignUpForm = () => {
       {
         onSuccess: () => {
           router.push("/dashboard");
+        },
+        onError: (ctx) => {
+          console.log(ctx.error);
+          if (ctx.error.code === "USER_ALREADY_EXISTS") {
+            toast.error("Email já cadastrado");
+            return;
+          }
+          toast.error("Erro ao criar a conta");
         },
       },
     );
